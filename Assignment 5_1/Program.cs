423199bf52169c438103bf93b5a614a8d5f250a8
Nicolai-Stangness <43436732+Nicolai-Stangness@users.noreply.github.com>
2018-10-15 17:28:24 +0200

    Add files via upload

---
+++ Assignment 5_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class quadcopter_generic
+    {
+        //I will just use the shorthand bt for bluetooth
+        protected bool ir = false;
+        protected bool rf = false;
+        protected bool bt = false;
+    }
+}
