1b1f25dcbd4ae59b5cfab64fe32939576ebbf25e
Nicolai-Stangness <43436732+Nicolai-Stangness@users.noreply.github.com>
2018-10-15 17:31:50 +0200

    Update Program.cs

--- Assignment 5_1/Program.cs
+++ Assignment 5_1/Program.cs
@@ -18,5 +18,40 @@ namespace Assignment_5_1
         protected bool ir = false;
         protected bool rf = false;
         protected bool bt = false;
+        
+        protected int snr_ir = 0;
+        protected int snr_rf = 0;
+        protected int snr_bt = 0;
+
+        protected int x = 0;
+        protected int y = 0;
+        protected int z = 0;
+
+        protected string model_id = "generic object";
+
+        public void select_comm_method()
+        {
+            if (snr_ir > snr_rf && snr_rf > snr_bt)
+            {
+                ir = true;
+            }
+            else if (snr_rf > snr_ir && snr_rf > snr_bt)
+            {
+                rf = true;
+            }
+            else
+            {
+                bt = true;
+            }
+        }
+        public void disp_id()
+        {
+            Console.WriteLine("Model type is {}", model_id);
+        }
+
+        public void set_position(int a, int b, int c)
+        {
+            x = a; y = b; c = z;
+        }
     }
 }
